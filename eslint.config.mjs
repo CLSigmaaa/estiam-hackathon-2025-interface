0d0a083d11c3c904bf816e401a80d7704a74d53f
Sigma <87021477+CLSigmaaa@users.noreply.github.com>
2025-07-02 15:31:54 +0200

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,13 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    settings: {
-      next: {
-        ignoreDuringBuilds: true
-      }
-    }
-  }
 ];
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   eslint: {
+    ignoreDuringBuilds: false, // true = ignore les erreurs ESLint lors du build
+  },
+  typescript: {
+    ignoreBuildErrors: false, // true = ignore les erreurs TypeScript lors du build
+  },
 };
 
 export default nextConfig;
